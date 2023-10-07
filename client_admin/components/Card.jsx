ee091f6e2b34ad374682748f1476b7be4df0c9de
Samuel-Brisio <samuelbrisio@outlook.com>
2023-10-06 23:02:41 -0300

    Copy the cards from the client, change colors

---
+++ client_admin/components/Card.jsx
@@ -0,0 +1,60 @@
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native"
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+function Card(props) {
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+            <LinearGradient
+                colors={['#5E93C9', '#5EB5C9']}
+                style={styles.border}
+                >
+                <View style={styles.card}>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>{props.title}</Text>
+                    </View>
+                    <View style={styles.body}>
+                        {props.children}
+                    </View>
+                </View>
+
+            </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({  
+    border: {
+        width: 150,
+        height: 150,
+        margin: 10,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    card: {
+        borderRadius: 20,
+        width: 146,
+        height: 146,
+        overflow: 'hidden'
+    },
+    header: {
+        flex: 1,
+        backgroundColor: '#5D92C8',
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        color: 'white',
+    },
+    body: {
+        flex: 3,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+})
+
+
+export {Card}
