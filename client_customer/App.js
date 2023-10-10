d0448a48ecc156f996bfbbb36f279a6f32f1e69a
Ga-Alves <gabrielalvesreis2gmail.com>
2023-10-09 22:00:41 -0300

    feat: deviceID

--- client_customer/App.js
+++ client_customer/App.js
@@ -4,7 +4,24 @@ import Routes from './utils/Routes';
 //Context
 import { Context } from "./context/AgendaBanhoContext";
 
+// storage
+import * as SecureStore from 'expo-secure-store';
+
+async function setDeviceID(key) {
+  const tkn = await SecureStore.getItemAsync(key)
+  
+  if(!tkn) {
+    console.log('No ID config yet.');
+    const backendID = '1'
+    const setTkn = await SecureStore.setItemAsync('id', backendID);
+  }
+  else{
+    console.log('Seu DeviceID é:', tkn);
+  }
+}
+
 export default function App() {
+  setDeviceID('id');
   return (
     <Context>
       <Routes/>
