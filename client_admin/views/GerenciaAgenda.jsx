4d0238f2b39f0b3e36e5c85152c98dec2c93319d
Ga-Alves <gabrielalvesreis2gmail.com>
2023-10-11 09:11:56 -0300

    feat: botão secundário e seção de funcionários tema de organizar agenda

--- client_admin/views/GerenciaAgenda.jsx
+++ client_admin/views/GerenciaAgenda.jsx
@@ -1,15 +1,42 @@
 // Native Components
+import { useEffect, useState } from "react";
 import { View, ScrollView, Text, StyleSheet, Dimensions } from "react-native"
 
 // components
 import { Footer } from "../components/Footer"
 import {textStyle} from '../utils/globalStyles'
+import { SecondaryButton } from "../components/SecondaryButton";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 
 function GerenciaAgenda() {
+
+    const [employees, setEmployees] = useState([])
+    const [selectedEmployee, setSelectedEmployee] = useState(-1)
+
+    function handleSelectEmployee(employee) {
+        setSelectedEmployee(employee.id);
+    }
+
+    useEffect(() => {
+        // simulando uma requisição de 1.5s de delay
+        setTimeout(() => {
+            setEmployees([
+                {
+                    id: 1,
+                    name: 'Fernanda'
+                },
+                {
+                    id: 5,
+                    name: 'Eduardo'
+                },
+            ])
+        }, 1500);
+    }, [])
+
+
     return (
         <ScrollView>
             <View style={styles.container}>
@@ -18,6 +45,13 @@ function GerenciaAgenda() {
                 </Text>
                 <View style={styles.body}>
                     <Text style={[textStyle.subtitle, textStyle.textColor, styles.subtitle]}>Funcionário</Text>
+
+                    <View style={styles.employees}>
+                        {employees && employees.map((employee) => 
+                            <SecondaryButton text={employee.name} isPress={employee.id == selectedEmployee} onPress={() => handleSelectEmployee(employee)}/>
+                        )}
+                    </View>
+
                 </View>
                 <Footer/>
             </View>
@@ -33,13 +67,18 @@ const styles = StyleSheet.create({
         minHeight: windowHeight,
     },
     body: {
-        flex: 1
+        flex: 1,
     },
     subtitle: {
         width: windowWidth/1.2,
-        marginTop: 20,
+        marginVertical: 20,
         borderBottomWidth: 1,
         borderColor: textStyle.textColor
+    },
+    employees: {
+        flexDirection: 'row',
+        gap: 5,
+        flexWrap: 'wrap'
     }
 })
 
