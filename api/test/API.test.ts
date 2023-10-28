0cdbceca0263876f22618f255b457381ee633f82
RaphaSilva <64025368+RaphaSilva470@users.noreply.github.com>
2023-10-28 19:29:16 -0300

    funcoes assincronas

--- api/test/API.test.ts
+++ api/test/API.test.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-test.skip("Deve cadastrar usuario", async function () {
+test("Deve cadastrar usuario", async function () {
 	await axios({
 		url: "http://localhost:3030/CadastroUser",
 		method: "post",
 		data: {
-			notifyToken: '131235690ddifrfff5',
+			notifyToken: '131235ddfrfff5',
 		}
 	});
 });
 	
-test("Deve cadastrar Pet", async function () {
+test.skip("Deve cadastrar Pet", async function () {
 	await axios({
 		url: "http://localhost:3030/CadastroPet",
 		method: "post",
