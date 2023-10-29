4750f0a9d0a7a7f0de003c4d8f35644a262ab15e
Samuel-Brisio <samuelbrisio@outlook.com>
2023-10-29 18:12:53 -0300

    Listar e Finalizar Ordem de serviço integrado

--- client_admin/views/OrdemServico.jsx
+++ client_admin/views/OrdemServico.jsx
@@ -12,16 +12,81 @@ import { RoundButton } from '../components/RoundButton';
 // context
 import { useState, useEffect } from 'react';
 
+// API
+import backend from '../services/BackEndAPI';
+
 import { serviceOrdersMock } from '../mock/serviceOrdersMock'
 
 function ServiceOrdemScreen(props) {
     const windowWidth = Dimensions.get('window').width;
     const TIMES = ["8:00", "8:50", "9:40", "10:30", "11:20", "13:10", "14:00", "14:50", "15:40"];
     const WEEK_DAYS = ['seg', 'ter', 'qua', 'qui', 'sex', 'sab'];
-    const WEEK_INDX = new Date().getDay() - 1 % 6;
+    const WEEK_INDX = Math.max(new Date().getDay() - 1 % 6, 0);
 
     const [week, setWeek] = useState(1)
     const [day, setDay] = useState(WEEK_DAYS[WEEK_INDX]);
+
+    const [serviceOrdersData, setServiceOrdersData] = useState([]);
+
+    function renameKeys(obj, newKeys) {
+        const entries = Object.keys(obj).map(key => {
+            const newKey = newKeys[key] || key;
+        
+            return {[newKey]: obj[key]};
+        });
+        
+        return Object.assign({}, ...entries);
+    }
+    
+    
+    
+    async function fetchData(dia, week){
+        const newKeys = {
+                        "horario": "time", 
+                        "completo": "completed",
+                        "data": "date",
+                        "id_banhista": "id_employee",
+                        "id_ordem": "id",  
+                        "nome_banhista": "resposable",
+                        "finalizacao": "finishing",
+                        "nome_pet": "title",
+                        "servicos": "services"
+        };
+        console.log(dia, week);
+            await backend.get(`ServiceOrders?day=${dia}&week=${week}`)
+            .then((response) => {
+                back_res = response.data
+                let serviceOrders = [];
+                back_res.forEach(element => {
+                    const SelectPatternOrder = renameKeys(element, newKeys);
+                    serviceOrders.push(SelectPatternOrder);
+                });
+                setServiceOrdersData(serviceOrders);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+
+    useEffect(() => {
+        fetchData(day, week);
+    }, [week, day])
+
+    const handleOrdemSubmit = async (id) => {
+        await backend.get(`FinalizaOrdemDeServico?id_ordem=${id}`)
+        .then(() => {
+            console.log(response.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    const handleDayWeekSelect = (day, week) => {
+        setDay(day);
+        setWeek(week);
+        fetchData(day, week);
+    }
     
     const schedule = (time, services, indx) => {
         return(
@@ -32,12 +97,13 @@ function ServiceOrdemScreen(props) {
                     {services && services.map((service, indx) => {
                        return <ServiceOrder
                             key={indx}
+                            id={service.id}
                             title={service.title}
                             services={service.services}
                             responsable={service.responsable}
                             total={service.total}
                             enabled={!service.completed}
-                            confirmOrder={()=>console.log('confirm callback', service.id)}/>
+                            confirmOrder={handleOrdemSubmit}/>
                     })
                     }
                     
@@ -46,20 +112,8 @@ function ServiceOrdemScreen(props) {
         )
     };
 
-    const [serviceOrdersData, setServiceOrdersData] = useState([]);
-
-    useEffect(() => {
-        // simulando uma requisição de 3s de delay
-        // para o dia <day> e semana <week>
-        setTimeout(() => {
-            setServiceOrdersData (serviceOrdersMock);
-            
-        }, 1500)
-    }, [week, day])
-
     return (
         <ScrollView>
-
             <View style={styles.container}>
                 <View style={styles.body}>
                     <View style={styles.cards}>
@@ -83,20 +137,20 @@ function ServiceOrdemScreen(props) {
 
                     <View style={[styles.selectDayAndWeek]}>
                         <Text>Semana: </Text>
-                        <RoundButton text='1' isSelected={week == '1'} setSelect={() => setWeek('1')}></RoundButton>
-                        <RoundButton text='2' isSelected={week == '2'} setSelect={() => setWeek('2')}></RoundButton>
-                        <RoundButton text='3' isSelected={week == '3'} setSelect={() => setWeek('3')}></RoundButton>
-                        <RoundButton text='4' isSelected={week == '4'} setSelect={() => setWeek('4')}></RoundButton>
-                        <RoundButton text='5' isSelected={week == '5'} setSelect={() => setWeek('5')}></RoundButton>
+                        <RoundButton text='1' isSelected={week == '1'} setSelect={() => handleDayWeekSelect(day, '1')}></RoundButton>
+                        <RoundButton text='2' isSelected={week == '2'} setSelect={() => handleDayWeekSelect(day, '2')}></RoundButton>
+                        <RoundButton text='3' isSelected={week == '3'} setSelect={() => handleDayWeekSelect(day, '3')}></RoundButton>
+                        <RoundButton text='4' isSelected={week == '4'} setSelect={() => handleDayWeekSelect(day, '4')}></RoundButton>
+                        <RoundButton text='5' isSelected={week == '5'} setSelect={() => handleDayWeekSelect(day, '5')}></RoundButton>
                     </View>
                     <View style={[styles.selectDayAndWeek]}>
                         <Text>Dia: </Text>
-                        <RoundButton text='seg' isSelected={day == 'seg'} setSelect={() => setDay('seg')}></RoundButton>
-                        <RoundButton text='ter' isSelected={day == 'ter'} setSelect={() => setDay('ter')}></RoundButton>
-                        <RoundButton text='qua' isSelected={day == 'qua'} setSelect={() => setDay('qua')}></RoundButton>
-                        <RoundButton text='qui' isSelected={day == 'qui'} setSelect={() => setDay('qui')}></RoundButton>
-                        <RoundButton text='sex' isSelected={day == 'sex'} setSelect={() => setDay('sex')}></RoundButton>
-                        <RoundButton text='sab' isSelected={day == 'sab'} setSelect={() => setDay('sab')}></RoundButton>
+                        <RoundButton text='seg' isSelected={day == 'seg'} setSelect={() => handleDayWeekSelect('seg', week)}></RoundButton>
+                        <RoundButton text='ter' isSelected={day == 'ter'} setSelect={() => handleDayWeekSelect('ter', week)}></RoundButton>
+                        <RoundButton text='qua' isSelected={day == 'qua'} setSelect={() => handleDayWeekSelect('qua', week)}></RoundButton>
+                        <RoundButton text='qui' isSelected={day == 'qui'} setSelect={() => handleDayWeekSelect('qui', week)}></RoundButton>
+                        <RoundButton text='sex' isSelected={day == 'sex'} setSelect={() => handleDayWeekSelect('sex', week)}></RoundButton>
+                        <RoundButton text='sab' isSelected={day == 'sab'} setSelect={() => handleDayWeekSelect('sab', week)}></RoundButton>
                     </View>
                 
                 </View>
